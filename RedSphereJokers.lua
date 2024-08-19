676b7b7ecb72e1c0cf7ae3a852950351281f79f8
RedSphereDev <chris@red-sphere.co.uk>
2024-08-20 00:49:32 +0100

    Added new Hand 'Countdown' with new Planet card of 'Moon'

--- RedSphereJokers.lua
+++ RedSphereJokers.lua
@@ -113,37 +113,92 @@ SMODS.Voucher{
 
 
 
-    local Card_calculate_joker_ref=Card.calculate_joker
-    function Card:calculate_joker(context)
-        
-        if self.ability.set == "Planet" and not self.debuff then
-            if context.joker_main then
-                -- important to order teh latest in the sequence first else it will only use  hubbles xmult value
-                if G.GAME.used_vouchers.v_red_jameswebb and self.ability.consumeable.hand_type == context.scoring_name then
-                    return {
-                        message = localize{type = 'variable', key = 'a_xmult', vars = {G.P_CENTERS.v_red_jameswebb.config.extra}},
-                        Xmult_mod = G.P_CENTERS.v_red_jameswebb.config.extra
-                    }
-                end
-                if G.GAME.used_vouchers.v_red_hubble and self.ability.consumeable.hand_type == context.scoring_name then
-                    return {
-                        message = localize{type = 'variable', key = 'a_xmult', vars = {G.P_CENTERS.v_red_hubble.config.extra}},
-                        Xmult_mod = G.P_CENTERS.v_red_hubble.config.extra
-                    }
-                end
-                
-            end
-        end
-        return Card_calculate_joker_ref(self,context)
-    end
-
-
-
-
-
-
-
+local Card_calculate_joker_ref=Card.calculate_joker
+function Card:calculate_joker(context)
     
+    if self.ability.set == "Planet" and not self.debuff then
+        if context.joker_main then
+            -- important to order the latest in the sequence first else it will only use  hubbles xmult value
+            if G.GAME.used_vouchers.v_red_jameswebb and self.ability.consumeable.hand_type == context.scoring_name then
+                return {
+                    message = localize{type = 'variable', key = 'a_xmult', vars = {G.P_CENTERS.v_red_jameswebb.config.extra}},
+                    Xmult_mod = G.P_CENTERS.v_red_jameswebb.config.extra
+                }
+            end
+            if G.GAME.used_vouchers.v_red_hubble and self.ability.consumeable.hand_type == context.scoring_name then
+                return {
+                    message = localize{type = 'variable', key = 'a_xmult', vars = {G.P_CENTERS.v_red_hubble.config.extra}},
+                    Xmult_mod = G.P_CENTERS.v_red_hubble.config.extra
+                }
+            end
+            
+        end
+    end
+    return Card_calculate_joker_ref(self,context)
+end
+
+
+
+SMODS.PokerHand {
+    key = 'Countdown',
+    name = 'CountDown',
+    chips = 80,
+    mult = 7,
+    l_chips = 35,
+    l_mult = 3,
+    example = {
+        { 'S_5', true },
+        { 'H_4', true },
+        { 'C_3', true },
+        { 'D_2', true },
+        { 'S_A', true },
+    },
+    loc_txt = {
+        name = "Countdown",
+        description = {
+            "All systems go! we're ready for launch",
+            "5, 4, 3, 2 and Ace",
+            "of any suit"
+        }
+    },
+    evaluate = function(parts, hand)
+        if next(parts._straight) then
+            local _strush = SMODS.merge_lists(parts._straight)
+            local countdown = true
+            for j = 1, #_strush do
+                local rank = SMODS.Ranks[_strush[j].base.value]
+                countdown = countdown and (rank.key == '5' or rank.key == '4' or rank.key == '3' or rank.key== '2' or rank.key == 'Ace')
+            end
+            if countdown then return {_strush} end
+        end
+    end
+}
+
+
+
+SMODS.Consumable {
+    set = "Planet",
+    key = "Moon",
+    pos = {x=3,y=0},
+    config = {hand_type = 'red_Countdown'},
+    loc_txt = {
+        name = "Moon",
+        text = {
+            "Level up",
+            "{C:attention}Countdown",
+            "{C:mult}+3{} Mult and",
+            "{C:chips}+35{} chips"
+        }
+    },
+    cost = 3,    
+    unlocked = true,
+    discovered = true,
+    atlas = "RedSphereAtlas",
+    can_use = true,
+    can_use = function(self, card)
+        return true
+    end
+}
 
 
    
